4da2b0dae79835ea7f11e848e611f9eb83839437
Sebastian Avila <sebastian.aavn@outlook.com>
2021-04-07 19:44:41 -0500

    arreglos navbar, service. adicion boton menu

--- js/main.js
+++ js/main.js
@@ -1,5 +1,10 @@
+// Variables
 const btt = document.querySelector('#btt');
+// const toggleMenu = document.querySelector('#btn-menu');
+const menu = document.querySelector('#menu');
+const nav = document.querySelector('#nav');
 
+// Back to Top Button
 window.onscroll = () => {
   scrollFuntion();
 }
@@ -18,4 +23,16 @@ function backtoTop() {
     left: 0,
     behavior: 'smooth'
   })
+}
+
+// Menu Button
+function toggleMenu() {
+  if(menu.style.display == 'none') {
+    menu.style.display = 'block';
+    nav.style.backgroundColor = 'rgba(0, 0, 0, 0)';
+    nav.style.backdropFilter = 'blur(5px)';
+  } else {
+    menu.style.display = 'none';
+    nav.style.backdropFilter = 'unset';
+  }
 }
